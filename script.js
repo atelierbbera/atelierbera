0a66d0d7f131bacf7633778cb617a49ba9a461ce
atelierbbera <berahasan18@gmail.com>
2026-06-25 02:16:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function toggleMenu() {
-    document.getElementById('side-menu').classList.toggle('active');
-}
-function toggleMenu() {
-    document.getElementById('side-menu').classList.toggle('active');
-}
-
 function addToCart(productName) {
-    alert(productName + " sepete eklendi!");
-    // Shopier linki veya sepet mantığı buraya
-}
-function addToCart(product) {
-    alert(product + " sepete eklendi!");
-    // Buraya sepet sayacını güncelleyen kodlar eklenebilir
+    alert(productName + " ürün sepetinize eklendi!");
 }
